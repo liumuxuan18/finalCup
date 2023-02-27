69a23483bde627d55263f9147c8523191698842b
ZarkHuang <st456915@gmail.com>
2023-02-27 21:18:54 +0800

    renew

--- src/App.js
+++ src/App.js
@@ -8,12 +8,6 @@ import Home from "./pages/home";
 import Footer from "./component/Footer";
 import NavBar from "./component/NavBar";
 import Fordummies from "./pages/fordummies";
-<<<<<<< HEAD
-
-=======
-// import './style/'
->>>>>>> 9e7b846dd7637a4df71e05c54117cd9240129502
-
 
 function App(){
     return (
@@ -23,12 +17,7 @@ function App(){
             {/* 上面不會動 */}
            <Routes>
             <Route path="finalCup/" element={<Home/>} />
-<<<<<<< HEAD
             <Route path="finalCup/fordummies" element={<Fordummies/>} /> {/* 懶人包 */}
-
-=======
-             <Route path="finalCup/fordummies" element={<Fordummies/>} />
->>>>>>> 9e7b846dd7637a4df71e05c54117cd9240129502
            </Routes>
             {/* 下面不會動 */}
            <Footer/>
